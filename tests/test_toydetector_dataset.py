c54410fc437a9fa8a7bbf4c6bd2f0142eae7dc66
Simone Balducci <simone.balducci00@gmail.com>
2024-08-10 14:02:13 +0200

    Add toyDet 5000 and 10000

--- tests/test_toydetector_dataset.py
+++ tests/test_toydetector_dataset.py
@@ -13,34 +13,74 @@ import CLUEstering as clue
 
 
 @pytest.fixture
-def toy_det():
+def toy_det_1000():
     '''
     Returns the dataframe containing the toy-detector dataset
+    with 1000 points.
     '''
-    return pd.read_csv("./test_datasets/toyDetector.csv")
+    return pd.read_csv("./test_datasets/toyDetector_1000.csv")
 
 
-def test_clustering(toy_det):
+@pytest.fixture
+def toy_det_5000():
+    '''
+    Returns the dataframe containing the toy-detector dataset
+    with 5000 points.
+    '''
+    return pd.read_csv("./test_datasets/toyDetector_1000.csv")
+
+
+@pytest.fixture
+def toy_det_10000():
+    '''
+    Returns the dataframe containing the toy-detector dataset
+    with 10000 points.
+    '''
+    return pd.read_csv("./test_datasets/toyDetector_1000.csv")
+
+
+def test_toy_1000(toy_det_1000):
     '''
     Checks that the output of the clustering is the one given by the
     truth dataset.
     '''
 
     # Check if the output file already exists and if it does, delete it
-    if os.path.isfile('./toy_det_output.csv'):
-        os.remove('./toy_det_output.csv')
+    if os.path.isfile('./toy_det_1000_output.csv'):
+        os.remove('./toy_det_1000_output.csv')
 
     c = clue.clusterer(5., 2.5, 1.)
-    c.read_data(toy_det)
+    c.read_data(toy_det_1000)
     c.run_clue()
-    c.to_csv('./', 'toy_det_output.csv')
+    c.to_csv('./', 'toy_det_1000_output.csv')
 
-    check_result('./toy_det_output.csv',
+    check_result('./toy_det_1000_output.csv',
                  './test_datasets/truth_files/toy_det_1000_truth.csv')
 
 
+def test_toy_5000(toy_det_5000):
+    '''
+    Checks that the output of the clustering is the one given by the
+    truth dataset.
+    '''
+
+    # Check if the output file already exists and if it does, delete it
+    if os.path.isfile('./toy_det_5000_output.csv'):
+        os.remove('./toy_det_5000_output.csv')
+
+    c = clue.clusterer(5., 2.5, 1.)
+    c.read_data(toy_det_5000)
+    c.run_clue()
+    c.to_csv('./', 'toy_det_5000_output.csv')
+
+    check_result('./toy_det_5000_output.csv',
+                 './test_datasets/truth_files/toy_det_5000_truth.csv')
+
 if __name__ == "__main__":
     c = clue.clusterer(5., 2.5, 1.)
-    c.read_data("./test_datasets/toyDetector.csv")
+    c.read_data("./test_datasets/toyDetector_10000.csv")
+    c.input_plotter()
     c.run_clue()
+    print(c.n_clusters)
     c.cluster_plotter()
+    # c.to_csv('./test_datasets/truth_files/', 'toy_det_5000_output.csv')
